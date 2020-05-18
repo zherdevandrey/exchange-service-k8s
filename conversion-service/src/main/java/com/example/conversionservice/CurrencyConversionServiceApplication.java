2645416bd1c29ca2ce039ff2e814dd67005c2bdd
zherdev <mr.zherdev1992@gmaik.com>
2020-05-18 22:27:54 +0300

    deployed to k8s

---
+++ conversion-service/src/main/java/com/example/conversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.conversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.example")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
---
+++ conversion-service/src/main/java/com/example/conversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,18 @@
+package com.example.conversionservice.resource;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "currency-exchange", url = "${EXCHANGE_SERVICE_SERVICE_HOST:http://localhost}:8000")//
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_URI:http://localhost}:8000")//Kubernetes Service Name
+//@FeignClient(name = "exchange-service")//Kubern
+//@RibbonClient(name = "exchange-service")//Kubernetes Service Name
+public interface CurrencyExchangeServiceProxy {
+	//http://localhost:8000/currency-exchange/from/USD/to/INR
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
+			@PathVariable("to") String to);
+}
